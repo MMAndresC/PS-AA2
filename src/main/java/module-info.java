a038c6bc9e462b9461b5a234da56cd6bfb878971
Mar <mandrescasabon@hotmail.com>
2025-04-06 18:30:49 +0200

    :wrench: config dependencies gson, retrofit & rxjava

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,7 +7,15 @@ module com.svalero.psaa2 {
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
     requires eu.hansolo.tilesfx;
+    requires static lombok;
+    requires retrofit2;
+    requires com.google.gson;
+    requires retrofit2.converter.gson;
+    requires retrofit2.adapter.rxjava3;
+    requires io.reactivex.rxjava3;
 
+    //MUY IMPORTANTE poner esto o GSON no tendra acceso a las clases de domain y fallara
+    opens com.svalero.psaa2.domain to com.google.gson;
     opens com.svalero.psaa2 to javafx.fxml;
     exports com.svalero.psaa2;
 }
