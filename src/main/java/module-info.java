489c8dd07252a5821c07b04c9d3957a64f023883
Mar <mandrescasabon@hotmail.com>
2025-04-07 21:05:28 +0200

    :wrench: add/remove dependencies

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,19 +3,21 @@ module com.svalero.psaa2 {
     requires javafx.fxml;
     requires javafx.web;
 
-    requires org.controlsfx.controls;
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
     requires static lombok;
     requires retrofit2;
     requires com.google.gson;
     requires retrofit2.converter.gson;
     requires retrofit2.adapter.rxjava3;
     requires io.reactivex.rxjava3;
+    requires java.logging;
+    requires okhttp3;
 
     //MUY IMPORTANTE poner esto o GSON no tendra acceso a las clases de domain y fallara
     opens com.svalero.psaa2.domain to com.google.gson;
     opens com.svalero.psaa2 to javafx.fxml;
     exports com.svalero.psaa2;
+    exports com.svalero.psaa2.controller;
+    opens com.svalero.psaa2.controller to javafx.fxml;
 }
