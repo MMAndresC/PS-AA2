abf301b434cc9e96364c3c775197d373282567bb
Mar <mandrescasabon@hotmail.com>
2025-04-07 21:09:38 +0200

    :construction: add clan tableView

---
+++ src/main/java/com/svalero/psaa2/controller/AppController.java
@@ -0,0 +1,13 @@
+package com.svalero.psaa2.controller;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AppController implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
